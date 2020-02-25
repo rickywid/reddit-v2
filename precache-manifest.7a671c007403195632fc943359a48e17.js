348ffc1de827e39080b91dd707f7204528003e28
rickywid <rickywid@hotmail.com>
2020-02-25 17:04:42 -0500

    Updates

---
+++ precache-manifest.7a671c007403195632fc943359a48e17.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "48ba381c1a7d1c933dc4",
+    "url": "/reddit-v2/static/css/main.e365c7bd.chunk.css"
+  },
+  {
+    "revision": "48ba381c1a7d1c933dc4",
+    "url": "/reddit-v2/static/js/main.48ba381c.chunk.js"
+  },
+  {
+    "revision": "8859d0522f7359347231",
+    "url": "/reddit-v2/static/css/1.dc7a0df2.chunk.css"
+  },
+  {
+    "revision": "8859d0522f7359347231",
+    "url": "/reddit-v2/static/js/1.8859d052.chunk.js"
+  },
+  {
+    "revision": "62511f4145d9aee8882c",
+    "url": "/reddit-v2/static/js/runtime~main.62511f41.js"
+  },
+  {
+    "revision": "9bda81de16f62a6f77210f82b32e21e0",
+    "url": "/reddit-v2/static/media/project.9bda81de.svg"
+  },
+  {
+    "revision": "0c7342106659e90017e5063f0179681a",
+    "url": "/reddit-v2/static/media/upload.0c734210.svg"
+  },
+  {
+    "revision": "2b21363c86bdf9bfcc87409f7a41ddfd",
+    "url": "/reddit-v2/static/media/falling-star.2b21363c.svg"
+  },
+  {
+    "revision": "c6913961a3199a88e27d855ed398e8d5",
+    "url": "/reddit-v2/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reddit-v2/precache-manifest.aabfacd72858976e52ff61f56b8a22c8.js"
+  "/reddit-v2/precache-manifest.7a671c007403195632fc943359a48e17.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/reddit-v2/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
