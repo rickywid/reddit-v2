3bf419675def20b9abc9a859fef6963e55082a85
rickywid <rickywid@hotmail.com>
2020-02-26 18:24:07 -0500

    Updates

---
+++ precache-manifest.c46a0616e6b274973df3d1e6a940ec4d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "31cb72262bfcb7a667c1",
+    "url": "/dash/static/css/main.67d2ab85.chunk.css"
+  },
+  {
+    "revision": "31cb72262bfcb7a667c1",
+    "url": "/dash/static/js/main.31cb7226.chunk.js"
+  },
+  {
+    "revision": "89907389d010a4d8761a",
+    "url": "/dash/static/css/1.dc7a0df2.chunk.css"
+  },
+  {
+    "revision": "89907389d010a4d8761a",
+    "url": "/dash/static/js/1.89907389.chunk.js"
+  },
+  {
+    "revision": "a3b4d4e247377154dc4b",
+    "url": "/dash/static/js/runtime~main.a3b4d4e2.js"
+  },
+  {
+    "revision": "0c7342106659e90017e5063f0179681a",
+    "url": "/dash/static/media/upload.0c734210.svg"
+  },
+  {
+    "revision": "e71d290b2e203e585c79da657bb31552",
+    "url": "/dash/static/media/cubes.e71d290b.svg"
+  },
+  {
+    "revision": "58eefd815174e7eec664bad1286152eb",
+    "url": "/dash/static/media/layers.58eefd81.svg"
+  },
+  {
+    "revision": "b831f4d49b5d7d7b0e0586ae753b39af",
+    "url": "/dash/static/media/puzzle.b831f4d4.svg"
+  },
+  {
+    "revision": "5a2734389f839aba55c1903e562b283d",
+    "url": "/dash/static/media/circle.5a273438.svg"
+  },
+  {
+    "revision": "26457186aeab1da1b90d88fa72a26f9c",
+    "url": "/dash/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reddit-v2/precache-manifest.d9571f03abe30a7f8036e919a06bc8a8.js"
+  "/dash/precache-manifest.c46a0616e6b274973df3d1e6a940ec4d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/reddit-v2/index.html", {
+workbox.routing.registerNavigationRoute("/dash/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
